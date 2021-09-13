5d9a4451b860fc27f127fb89933a0f7b739ae00b
Samuel Osinloye <psalmueloye@gmail.com>
2021-09-13 18:13:36 +0100

    Add project files.

---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class HotelListingDbContext:DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Germany",
+                    ShortName = "DE"
+                },
+                new Country
+                { 
+                    Id = 2, 
+                    Name = "Bulgaria", 
+                    ShortName = "BG" 
+                },
+                new Country
+                {
+                    Id=3,
+                    Name="Denmark",
+                    ShortName="DK"
+                },
+                new Country
+                {
+                    Id=4,
+                    Name="Czechia",
+                    ShortName="CZ"
+                },
+                new Country
+                {
+                    Id=5,
+                    Name="Belgium",
+                    ShortName="BE"
+                }
+                );
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id=1,
+                    Name="Sheraton",
+                    Address="30, Mobolaji Bank Anthony Way, Airport Road, Ikeja GRA",
+                    Rating=4.50,
+                    CountryId = 2,
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Wheatbaker",
+                    Address = "4, Onitolo Road, Off Gerrard Road",
+                    Rating = 3.70,
+                    CountryId = 1,
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Transcorp Hilton",
+                    Address = "30, Mobolaji Bank Anthony Way, Airport Road, Ikeja GRA",
+                    Rating = 4.65,
+                    CountryId = 5,
+                },
+                new Hotel
+                {
+                    Id = 4,
+                    Name = "Hotel Presidential",
+                    Address = "5141, Aba Road, GRA Phase 2, Rumuadaolu",
+                    Rating = 4.10,
+                    CountryId = 2,
+                },
+                new Hotel
+                {
+                    Id = 5,
+                    Name = "Golden Royale",
+                    Address = "10, Bisala Road, Independence Layout",
+                    Rating = 3.50,
+                    CountryId = 3,
+                }
+                );
+        }
+       
+    }
+}
---
+++ Migrations/20210913171134_SeedModelData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class SeedModelData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Germany", "DE" },
+                    { 2, "Bulgaria", "BG" },
+                    { 3, "Denmark", "DK" },
+                    { 4, "Czechia", "CZ" },
+                    { 5, "Belgium", "BE" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 2, "4, Onitolo Road, Off Gerrard Road", 1, "Wheatbaker", 3.7000000000000002 },
+                    { 1, "30, Mobolaji Bank Anthony Way, Airport Road, Ikeja GRA", 2, "Sheraton", 4.5 },
+                    { 4, "5141, Aba Road, GRA Phase 2, Rumuadaolu", 2, "Hotel Presidential", 4.0999999999999996 },
+                    { 5, "10, Bisala Road, Independence Layout", 3, "Golden Royale", 3.5 },
+                    { 3, "30, Mobolaji Bank Anthony Way, Airport Road, Ikeja GRA", 5, "Transcorp Hilton", 4.6500000000000004 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
